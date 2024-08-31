f7edc44830d692d25e1b4e29df7a7edb504ad7dc
AshishXGitHub <ashishkadu2123@gmail.com>
2024-09-01 00:30:38 +0530

    65%-commit by Ashish

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -9,130 +9,121 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-// import { Input } from "@/components/ui/input";
-// import { Textarea } from "@/components/ui/textarea";
-import { chatSession } from "@/utils/GeminiAIModel";
-import { MockInterview } from "@/utils/schema";
-import {v4 as uuid4} from 'uuid';
-import { db } from "@/utils/db";
-import { useUser } from "@clerk/nextjs";
-import moment from 'moment';
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { useRouter } from "next/navigation";
+import { LoaderCircle } from "lucide-react";
+import { chatSession } from "@/utils/GeminiAIModel";
+import {v4 as uuid4} from 'uuid';
+import { useUser } from "@clerk/nextjs";
+import moment from "moment";
+import { MockInterview } from "@/utils/schema";
+import { db } from "@/utils/db";
 
-const AddNewInterview = () => {
+function AddNewInterview() {
   const [openDialog, setOpenDialog] = useState(false);
-    const [jobPosition,setJobPosition]=useState();
-    const [jobDesc,setJobDesc] = useState();
-    const [jobExperience,setJobExperience]= useState();
-    const [loading,setLoading] = useState(false);
-    const [jsonResponse,setJsonResponse] = useState([]);
-    const{user} = useUser();
-    const router = useRouter();
+  const [jobPosition,setJobPosition]=useState();
+  const [jobDesc,setJobDesc] = useState();
+  const [jobExperience,setJobExperience]= useState();
+  const [loading,setLoading] = useState(false);
+  const [jsonResponse,setJsonResponse] = useState([]);
+  const{user} = useUser();
+  const router = useRouter();
 
+  const onSubmit=async(e)=>{
+    setLoading(true)
+    e.preventDefault();
+    const InputPrompt = "Job position:" +jobPosition+" ,job Description :" +jobDesc +" ,Years of Experience:" +jobExperience +" Depends on this information please give me 5 interview question and answer in json format give question and answer as fiels in json  format";
+    const result=await chatSession.sendMessage(InputPrompt);
+    const MockJsonResp = (result.response.text()).replace('```json','').replace('```','')
+    // console.log(JSON.parse(MockJsonResp));
+    setJsonResponse(MockJsonResp);
+    // console.log(MockJsonResp)
 
-    const onSubmit=async(e)=>{
-        e.preventDefault();
-        const InputPrompt = "Job position:" +jobPosition+" ,job Description :" +jobDesc +" ,Years of Experience:" +jobExperience +" Depends on this information please give me five interview question and answer in json format give question and answer as fiels in json  format";
-        const result=await chatSession.sendMessage(InputPrompt);
-        const MockJsonResp = (result.response.text()).replace('```json','').replace('```','')
-        console.log(JSON.parse(MockJsonResp));
-        setJsonResponse(MockJsonResp);
-        // console.log(MockJsonResp)
-
-      if(MockJsonResp)
-      {
-      const resp=await db.insert(MockInterview)
-      .values({
-        mockId:uuid4(),
-        jsonMockResp:MockJsonResp,
-        jobPosition:jobPosition,
-        jobDesc:jobDesc,
-        jobExperience:jobExperience,
-        createdBy:user?.primaryEmailAddress?.emailAddress,
-        createdAt:moment().format('DD-MM-YYYY')
-      }).returning({mockId:MockInterview.mockId})
-      console.log("Inserted ID: ",resp);
-      if(resp){
-        setOpenDialog(false);
-        router.push('/dashboard/interview/'+resp[0]?.mockId)
-      }
-    }
-    else{
-      console.log("Error inserting mock interview");
-    }
-        setLoading(false);
-
-    }
-
+  if(MockJsonResp)
+  {
+  const resp=await db.insert(MockInterview)
+  .values({
+    mockId:uuid4(),
+    jsonMockResp:MockJsonResp,
+    jobPosition:jobPosition,
+    jobDesc:jobDesc,
+    jobExperience:jobExperience,
+    createdBy:user?.primaryEmailAddress?.emailAddress,
+    createdAt:moment().format('DD-MM-YYYY')
+  }).returning({mockId:MockInterview.mockId})
+  console.log("Inserted ID: ",resp);
+  if(resp){
+    setOpenDialog(false);
+    router.push('/dashboard/interview/'+resp[0]?.mockId)
+  }
+}
+else{
+  console.log("Error inserting mock interview");
+}
+    setLoading(false);
+  }
   return (
     <div>
       <div
-        className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transation-all"
+        className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all"
         onClick={() => setOpenDialog(true)}
       >
-        <h2 className=" text-lg text-center">+Add New</h2>
+        <h2 className="text-lg text-center">+ Add New</h2>
       </div>
-      <form onSubmit={onSubmit}>
-                <div>
-                    <h2>Add Details about your job position/role ,Job Description</h2>
-
-                    <div className='mt-7 my-3'>
-                        <label >Job Role / Job Position</label>
-                        <input placeholder="Ex. Full Stack Developer" required onChange={(event)=>setJobPosition(event.target.value)}/>
-                    </div>
-                    
-                    <div className=' my-3'>
-                        <label >Job Description / Tech Stack (In Short)</label>
-                        <textarea placeholder="Ex. React,Angular,Nodejs" required onChange={(event)=>setJobDesc(event.target.value)}/>
-                    </div>
-                    
-                    <div className=' my-3'>
-                        <label >Years of Experience</label>
-                        <input placeholder="Ex. 5" type="number" max="100" required onChange={(event)=>setJobExperience(event.target.value)}/>
-                    </div>
-                </div>
-                <div>
-                <Button type="button" varient="ghost" onClick={()=>setOpenDialog(false)}>Cancle</Button>
-                <Button onClick={onSubmit}>Start Interview</Button>
-              </div>
-              </form>
-              
-      <Dialog Open={openDialog}>
-        <DialogContent>
+      <Dialog open={openDialog}>
+        <DialogContent className="max-w-2xl">
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle className="text-2xl">
+              Tell us more about your job interviwing
+            </DialogTitle>
             <DialogDescription>
-              {/* <form onSubmit={onSubmit}>
-                <div>
-                    <h2>Add Details about your job position/role ,Job Description</h2>
+              <form onSubmit={onSubmit}>
+              <div className="">
+                <h2 className="">
+                  Add Details about your job position/role ,Job Description
+                </h2>
 
-                    <div className='mt-7 my-3'>
-                        <label >Job Role / Job Position</label>
-                        <Input placeholder="Ex. Full Stack Developer" required onChange={(event)=>setJobPosition(event.target.value)}/>
-                    </div>
-                    
-                    <div className=' my-3'>
-                        <label >Job Description / Tech Stack (In Short)</label>
-                        <Textarea placeholder="Ex. React,Angular,Nodejs" required onChange={(event)=>setJobDesc(event.target.value)}/>
-                    </div>
-                    
-                    <div className=' my-3'>
-                        <label >Years of Experience</label>
-                        <Input placeholder="Ex. 5" type="number" max="100" required onChange={(event)=>setJobExperience(event.target.value)}/>
-                    </div>
+                <div className="mt-7 my-3">
+                  <label>Job Role/Job Position</label>
+                  <Input placeholder="Ex. Full Stack Developer" required onChange={(event)=>setJobPosition(event.target.value)}/>
                 </div>
-                <div>
-                <Button type="button" varient="ghost" onClick={()=>setOpenDialog(false)}>Cancle</Button>
-                <Button type="submit" className='animate-spin' disable={loading}> {loading?<><LoadingCircle>'Generating From AI'</>:'Start  Iterview' }</Button>
+                <div className="my-3">
+                  <label>Job Description / Tech Stack (In Short)</label>
+                  <Textarea placeholder="Ex. React,Angular,Nodejs" required onChange={(event)=>setJobDesc(event.target.value)}/>
+                </div>
+                <div className="my-3">
+                  <label>Years of Experience</label>
+                  <Input
+                    placeholder="Ex. 5"
+                    type="number"
+                    min="0"
+                    max="50"
+                    required
+                    onChange={(event)=>setJobExperience(event.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="flex gap-5 justify-end">
+                <Button type="button" variant="ghost" onClick={() => setOpenDialog(false)}>
+                  Cancel
+                </Button>
+                <Button type="submit" disabled={loading}>
+                  {loading?
+                  <>
+                  <LoaderCircle className="animate-spin"/>'Generating from AI'
+                  </>:'Start Interview'
+                }
+               
+                </Button>
               </div>
               </form>
-               */}
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
       </Dialog>
     </div>
   );
-};
-
+}
 export default AddNewInterview;
--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -1,32 +1,89 @@
-"use client"
-import { MockInterview } from '@/utils/schema'
-import React from 'react'
-import { useEffect,useState } from 'react'
-import { db } from '@/utils/db'
-import { eq } from 'drizzle-orm'
-import Webcam from 'react-webcam'
+"use client";
+import { MockInterview } from "@/utils/schema";
+import React from "react";
+import { useEffect, useState } from "react";
+import { db } from "@/utils/db";
+import { eq } from "drizzle-orm";
+import Webcam from "react-webcam";
+import { Lightbulb, WebcamIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
-const Interview = ({params}) => {
+const Interview = ({ params }) => {
+  const { interviewData, setInterviewData } = useState();
+  const [webCamEnabled, setWebCamEnabled] = useState(false);
 
-  const {interviewData,setInterviewData} = useState();
-  useEffect(()=>{
-    console.log(params.interviewId)
+  useEffect(() => {
+    console.log(params.interviewId);
     GetInterviewDetails();
-  },[])
+  }, []);
 
-  const GetInterviewDetails=async()=>{
-    const result = await db.select().from(MockInterview)
-    .where(eq(MockInterview.mockId,params.interviewId))
+  /**
+   * Used to Get Interview details by MockId/Interview Id
+   */
+  const GetInterviewDetails = async () => {
+    const result = await db
+      .select()
+      .from(MockInterview)
+      .where(eq(MockInterview.mockId, params.interviewId));
     console.log(result);
     setInterviewData(result[0]);
-  }
+  };
 
   return (
-    <div className='my-10 flex justify-center flex-col items-center'>
-      <h2 className='font-bold text-2xl'>Let's Get Started</h2>
-      <div><Webcam/></div>
+    <div className="my-10">
+      <h2 className="font-bold text-2xl">Let's Get Started</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="flex flex-col my-5 gap-5 ">
+          <div className="flex flex-col p-5 rounded-lg border gap-5">
+            <h2 className="text-lg">
+              <strong>Job Role/ Job Position:</strong>
+            </h2>
+            <h2 className="text-lg">
+              <strong>Job Description/ Tech Stack:</strong>
+            </h2>
+            <h2 className="text-lg">
+              <strong>Years of Experience:</strong>
+            </h2>
+          </div>
+          <div className="p-5 border rounded-lg border-yellow-300 bg-yellow-100">
+            <h2 className="flex gap-2 items-center text-yellow-500">
+              <Lightbulb /> <strong>Information</strong>
+            </h2>
+            <h2 className="mt-3 text-yellow-500">
+              Enable Video Web Cam and Microphone to Start your AI Generated
+              Mock Interview, It Has 5 questions which you can answer and at the
+              last you will get the report on the basis of youe answer.NOTE:We
+              never record your video, Web Cam access you can disable at any
+              time of you want
+            </h2>
+          </div>
+        </div>
+        <div>
+          {webCamEnabled ? (
+            <Webcam
+              onUserMedia={() => setWebCamEnabled(true)}
+              onUserMediaError={() => setWebCamEnabled(false)}
+              mirrored={true}
+              style={{
+                height: 300,
+                width: 300,
+              }}
+            />
+          ) : (
+            <>
+              <WebcamIcon className="h-72 w-full my-7 p-20 bg-secondary rounded-lg border" />
+              <Button  onClick={() => setWebCamEnabled(true)} className="w-full">
+                Enable Web Cam and Microphone
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
+      <div className="flex justify-end items-end">
+      <Button>Start Interview</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Interview
+export default Interview;
